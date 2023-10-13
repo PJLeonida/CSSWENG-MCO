1e767c2c09112d216e32f106b7225a09e0b08211
PJLeonida <patrick_josh_c_leonida@dlsu.edu.ph>
2023-10-14 00:13:55 +0800

    Initial db integration and connecting

---
+++ db/Employees.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const employeeSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        require: true 
+    },
+
+    lastName: {
+        type: String,
+        require: true
+    },
+
+    position: {
+        type: String, 
+        require: true
+    },
+
+    rate: {
+        type: Number,
+        require: true
+    },
+
+    totalrate: {
+        type: Number,
+        require: true
+    },
+
+    deployment: {
+        type: Schema.Types.ObjectId,
+        ref: 'EmpDeployment',
+    },
+
+    notes: {
+        type: String
+    }
+})
+
+const Employee = mongoose.model('Employee', employeeSchema)
+
+module.exports = Employee
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,17 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose')
+
+const dburl = process.env.db_url ?? ""
+
+const db_operations = {
+
+    connect: async function () {
+        await mongoose.connect(dburl, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+        console.log('Connected to: ' + dburl);
+    }
+
+}
+
+module.exports = db_operations;
