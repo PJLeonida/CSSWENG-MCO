d7a433b24c5fd8e37a529c1e57951733e8dd5d12
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2023-11-06 01:57:23 +0800

    Update renderer.js

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,1 +1,13 @@
-console.log("Hello!")
+console.log("The application is running!")
+
+function clickedLoginBtn() {
+    console.log("Login button clicked!")
+}
+
+function clickedRegisterBtn() {
+    console.log("Register button clicked!")
+}
+
+function clickedLogoutBtn() {
+    console.log("Logout button clicked!")
+}
