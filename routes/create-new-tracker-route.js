4b6c73a4d37b22c966a48b3ab0daebf7629e3dd1
Derek Bautista <derek_roi_t_bautista@dlsu.edu.ph>
2023-11-21 15:00:20 +0800

    Cleaned my crete new tracker route for merge

--- routes/create-new-tracker-route.js
+++ routes/create-new-tracker-route.js
@@ -8,75 +8,9 @@ const employees = require('../server/schema/Employees');
 
 router.post('/', async (req, res) => {
     
-    try {
-        const newProject = {
-            newp_projectName,
-            newp_projectDesc
-        } = req.body
-        
-        console.log('GUMANA KA PLS IIYAK NA AKO')
-    } catch (error) {
-        console.error('Error registering:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-
-        // If the user is trying to register
-       
-        /*
-    projectName
-
-    totalmanpower
-
-    employees
-
-    utilization
-
-})
-*/
+  
 })
             
-
-/*
-Theres a create new project and a create new employee
-
-In project, just insert it with default values.
-
-When an employee is created with an assigned project:
-1. Create Employee
-FirstName: input
-
-    lastName: input
-
-    position: input
-
-    rate: input(same as role in EmpDeploy)
-
-    totalrate: input
-
-2. create deployment
-     employee: employeeID,
-
-    firstName: input ,
-
-    lastName: input ,
-
-    projectassign: input project's name id (projectID).,
-    projectID = projects.find(ProjectName).getProjectID
-
-    role: input
-
-    deploymenthrs: input
-
-3. Update Project
-
-    totalmanpower: will be changed by +1
-
-    employees: not sure how to display this, but if ever to get the list of employees of a project:
-    empDeployment: findAll(projectassign = inputted projectName's id)
-
-})
-*/
-
 
 router.get('/', async (req, res) => {
     res.render('landing-page', {
