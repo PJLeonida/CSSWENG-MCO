25e58a81799322571f3f20b6ad08cdbc7c1fa1a5
ImAmeliaJoyce <amelia_abenoja@dlsu.edu.ph>
2023-11-23 04:18:18 +0800

    Added save/cancel modals

--- routes/template-project-tracker-route.js
+++ routes/template-project-tracker-route.js
@@ -85,4 +85,15 @@ router.get('/:projectID', async (req, res) => {
     });
 });
 
+
+// Use for design purposes only
+router.get('/', (req, res) => {
+    res.render('landing-page', { 
+        pageTitle: 'Project Tracker',
+        partial: 'template-project-tracker',
+        activePage: 'account-settings',
+        script: '/static/js/template-project-tracker.js'
+    });
+});
+
 module.exports = router;
